8e6be5db2521b0b1332c9ed9e840c35acb652cde
Sergey Balashov <waisted.deals.07@icloud.com>
2023-11-26 16:09:21 -0800

    Initial Commit

---
+++ LoginAppNew/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginAppNew
+//
+//  Created by Sergey Balashov on 11/26/23.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
